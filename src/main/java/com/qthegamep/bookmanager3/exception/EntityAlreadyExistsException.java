e19b9120ee3fc1744ef4cb2880b93ad77c99c7a3
Nikita Koliadin <qThegamEp@gmail.com>
2018-10-14 15:32:08 +0300

    Updated JavaDoc

--- src/main/java/com/qthegamep/bookmanager3/exception/EntityAlreadyExistsException.java
+++ src/main/java/com/qthegamep/bookmanager3/exception/EntityAlreadyExistsException.java
@@ -1,10 +1,15 @@
 package com.qthegamep.bookmanager3.exception;
 
 /**
- * This exception should be thrown when trying to add an entity to the database whet it already exists.
+ * This exception should be thrown when trying to add an entity to the database when it already exists.
  */
 public class EntityAlreadyExistsException extends RuntimeException {
 
+    /**
+     * This is the constructor that calls the super constructor and passes the error message to is.
+     *
+     * @param message to be displayed or logged.
+     */
     public EntityAlreadyExistsException(String message) {
         super(message);
     }
